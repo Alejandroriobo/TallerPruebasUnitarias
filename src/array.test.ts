a7ceb76bada90e140c9bb78f32780d922461cdc8
juanmanuelquiroga <mq137267@gmail.com>
2024-04-24 15:45:47 -0500

    pruebas arrayts

---
+++ src/array.test.ts
@@ -0,0 +1,33 @@
+import { arrayContainsValue, arrayNotContainsValue } from './array';
+
+describe('arrayContainsValue function', () => {
+    it('should return true if the array contains the value', () => {
+        const arr = ['value1', 'value2', 'value3'];
+        const value = 'value1';
+        const result = arrayContainsValue(arr, value);
+        expect(result).toBe(true);
+    });
+
+    it('should return false if the array does not contain the value', () => {
+        const arr = ['value2', 'value3'];
+        const value = 'value1';
+        const result = arrayContainsValue(arr, value);
+        expect(result).toBe(false);
+    });
+});
+
+describe('arrayNotContainsValue function', () => {
+    it('should return false if the array contains the value', () => {
+        const arr = ['value1', 'value2', 'value3'];
+        const value = 'value1';
+        const result = arrayNotContainsValue(arr, value);
+        expect(result).toBe(false);
+    });
+
+    it('should return true if the array does not contain the value', () => {
+        const arr = ['value2', 'value3'];
+        const value = 'value1';
+        const result = arrayNotContainsValue(arr, value);
+        expect(result).toBe(true);
+    });
+});
